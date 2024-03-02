8ff645460ecd9d28670d674159cf2652e0feac31
reptila <r3ptil4@protonmail.com>
2024-03-02 22:42:48 +0200

    update lectie

--- src/components/pages/lectii/lectia1.js
+++ src/components/pages/lectii/lectia1.js
@@ -9,20 +9,22 @@ export default function Lectia1() {
         <div className='text'>
           <p className='text-lectie'>
             <span className='span-color'>Simbolismul</span> este un curent literar apărut în Franţa, ca reacţie împotriva parnasianismului, a romantismului retoric 
-            şi a naturalismului, promovând conceptul de poezie modernă. În concepţia simboliştilor, descripţia rece,parnasiană, 
-            ca şi observaţia meticuloasă, naturalistă, urmărind să decupeze“o felie de viaţă”, nu pot surprinde altceva decât o 
+            şi a naturalismului, promovând conceptul de poezie modernă. În concepţia simboliştilor, descripţia rece, parnasiană, 
+            ca şi observaţia meticuloasă, naturalistă, urmărind să decupeze “o felie de viaţă”, nu pot surprinde altceva decât o 
             realitate superficială, o lume a aparenţelor. Considerat din perspectiva social-istorică, simbolismul apare ca produs 
             şi expresie a stării de spirit generate de agravarea contradicţiilor societăţii capitaliste de la sfârşitul secolului 
             al XIX-lea.
           </p>
           <p className='text-lectie'>
             Adesea se consideră că poeți ca Charles Baudelaire, Arthur Rimbaud și Paul Verlaine fac parte din acest 
-            curent, dar Stephane Mallarmé este cel care îl întruchipează cel mai bine în poezie.
+            curent, dar Stephane Mallarmé este cel care îl întruchipează cel mai bine în poezie. S-au impus trei direcții fundamentale: 
+            Școala simbolistă, sub patronajul lui Mallarme; Școala decadentă, care se revendică din Baudelaire, având ca reprezentanți 
+            pe Paul Verlaine și Arthur Rimbaud; Școala instrumentalistă, susținută de Rene Ghil.
           </p>
           <p className='text-lectie'>
           Deși simbolismul francez a durat foarte puțin, curentul a reușit să formeze poezia modernă, 
           negând gândirea științifică, raționalistă. Dintre temele simboliste se remarcă: impalpabilul, 
-          angoasele, identitare ale Eului, imaginea femeii, arta pentru artă, decadenta.
+          angoasele, identitare ale Eului, imaginea femeii, arta pentru artă, decadența.
           </p>
           <p className='text-lectie'>
           Termenul de „simbolism” provine din cuvântul grecesc „symbolon ”, intrat în limbă prin filieră franceză. 
