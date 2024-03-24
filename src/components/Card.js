acfa5cdf3c533d9ec9d9f7c6b2cf3d6945357493
reptila <r3ptil4@protonmail.com>
2024-03-24 15:04:05 +0200

    media screen

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ export default function Card(props) {
       <li className='card-item'>
           <Link to={props.to} className='card-item-link'>
             <figure className='card-item-pic-wrap'>
-              <img src={props.src} alt="Card" className='card-item-img' height='160px' max-width='340px' />
+              <img src={props.src} alt="Card" className='card-item-img' />
             </figure>
             <div className='card-item-info'>
               <p className='card-item-text'>{props.text}</p>
--- src/components/pages/Lectii.js
+++ src/components/pages/Lectii.js
@@ -12,7 +12,7 @@ export default function Lectii() {
         <Card to="./lectia2" src="img/trasaturi.jpg" text="L3. Trăsături" alt="Trasaturi" />
         <Card to="./lectia3" src="img/steph.jpg" text="L4. Reprezentanți" alt="Reprezentanti" />
         <Card to="./lectia4" src="img/sro.jpg" text="L5. Simbolismul românesc" alt="Rom" />
-        <Card to="./lectia5" src="img/bacovia.jpg" text="L6. Simbolismul românesc - reprezentanți" alt="Rom" />
+        <Card to="./lectia5" src="img/bacovia.jpg" text="L6. Reprezentanți" alt="Rom" />
         <Card to="./lectia6" src="img/plumb.jpg" text="L7 Plumb - analiză" alt="plumb" />
       </ul>
     </div>
